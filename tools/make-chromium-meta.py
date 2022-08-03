9e74359409cd63d8ac3233d9d4b2eb8f68f166f6
Sourcery AI <>
2022-08-03 15:02:25 +0000

    'Refactored by Sourcery'

--- tools/make-chromium-meta.py
+++ tools/make-chromium-meta.py
@@ -22,9 +22,7 @@ with open(manifest_out_file) as f:
 
 manifest_out['version'] = version
 
-# Development build? If so, modify name accordingly.
-match = re.search('^\d+\.\d+\.\d+\.\d+$', version)
-if match:
+if match := re.search('^\d+\.\d+\.\d+\.\d+$', version):
     manifest_out['name'] += ' development build'
     manifest_out['short_name'] += ' dev build'
     manifest_out['browser_action']['default_title'] += ' dev build'
